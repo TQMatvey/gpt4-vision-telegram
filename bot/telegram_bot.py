31c79bc2b3b82688e95c47a6fafe2dbd65dd5fad
TQMatvey <maybeetube@gmail.com>
2023-12-10 15:03:36 +0700

    Refactor image handling to accept custom captions as prompts
    - Renamed `prompt` attribute to `default_prompt` for better clarity
    - Updated `get_image` to process the photo with either the user-provided caption or the default prompt
    - Enhanced `start` method to guide users on how to send a photo with a caption

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -23,10 +23,12 @@ class ChatGPTTelegramBot:
         """
         self.config = config
         self.openai = openai
-        self.prompt = "What Do you see on this image"
+        self.default_prompt = "What Do you see on this image"
         self.disallowed_message = "Sorry, you are not allowed to use this bot. You can check out the source code at https://github.com/TQMatvey/gpt4-vision-telegram"
 
-    async def is_allowed(self, config, update: Update, context: CallbackContext) -> bool:
+    async def is_allowed(
+        self, config, update: Update, context: CallbackContext
+    ) -> bool:
         if self.config["allowed_user_ids"] == "*":
             return True
 
@@ -42,44 +44,39 @@ class ChatGPTTelegramBot:
         Sends the disallowed message to the user.
         """
         await update.effective_message.reply_text(
-            text=self.disallowed_message,
-            disable_web_page_preview=True
+            text=self.disallowed_message, disable_web_page_preview=True
         )
 
     async def get_image(self, update: Update, context: CallbackContext) -> None:
+        # Check if the user is allowed to use this feature
         if not await self.is_allowed(self.config, update, context):
+            # If not, send a message and exit the function
             await self.send_disallowed_message(update)
             return
 
-        photo_link = await update.message.photo[-1].get_file()
+        # Retrieve the highest quality version of the photo sent by the user
+        photo_file = await update.message.photo[-1].get_file()
 
-        await update.message.reply_text(
-            self.openai.process_image(photo_link.file_path, self.prompt)
-        )
+        # Check if there's a caption provided with the photo
+        custom_prompt = update.message.caption
+
+        # If a caption is provided, use it as a prompt for image processing
+        if custom_prompt:
+            await update.message.reply_text(
+                self.openai.process_image(photo_file.file_path, custom_prompt)
+            )
+        else:
+            # Otherwise, use a default prompt defined elsewhere in your script
+            await update.message.reply_text(
+                self.openai.process_image(photo_file.file_path, self.default_prompt)
+            )
 
     async def start(self, update: Update, context: CallbackContext) -> None:
-        await update.message.reply_text("Send me an image! or set a prompt.")
-
-    async def save_prompt(self, update: Update, context: CallbackContext) -> None:
-        if not await self.is_allowed(self.config, update, context):
-            await self.send_disallowed_message(update)
-            return
-        
-        # Extract the user's prompt
-        self.prompt = update.message.text[8:]
-
-        # Send confirmation message
-        await context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="Custom Prompt set, you can now send an image.",
-        )
-
-    async def get_prompt(self, update: Update, context: CallbackContext) -> None:
-        if not await self.is_allowed(self.config, update, context):
-            await self.send_disallowed_message(update)
-            return
-        
-        await update.message.reply_text(f"Current prompt is: {self.prompt}.")
+        await update.message.reply_text("""
+📷 Please send me a photo with a caption.
+The Caption will serve as a prompt for Vision GPT\n
+Default prompt is: \"What Do you see on this image\"
+""")
 
     def run(self):
         application = (
@@ -90,8 +87,6 @@ class ChatGPTTelegramBot:
         )
 
         application.add_handler(CommandHandler("start", self.start))
-        application.add_handler(CommandHandler("prompt", self.save_prompt))
-        application.add_handler(CommandHandler("getprompt", self.get_prompt))
         application.add_handler(MessageHandler(filters.PHOTO, self.get_image))
 
         application.run_polling()
